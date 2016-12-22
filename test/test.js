377f5d504eeff4df2b903c346579ce824a7bff6b
Ivan Demidov <scrum@list.ru>
2016-12-22 16:26:51 +0300

    test(*): for plugins and arguments

--- test/test.js
+++ test/test.js
@@ -9,8 +9,8 @@ test('should retrun object', t => {
 	t.false(Array.isArray(postConfig()));
 });
 
-test('should return default config from package', t => {
-	const {postcss, posthtml} = postConfig();
+test('should return default config postcss from package', t => {
+	const {postcss} = postConfig();
 	const expected = {
 		postcss: {
 			from: 'test',
@@ -22,7 +22,9 @@ test('should return default config from package', t => {
 				autoprefixer: {
 					browsers: ['last 2 versions']
 				},
-				'postcss-at-rules-variables': {},
+				'postcss-at-rules-variables': {
+					atRule: ['@media']
+				},
 				'postcss-attribute-selector-prefix': {},
 				'postcss-banner': {},
 				'postcss-browser-reporter': {},
@@ -45,7 +47,15 @@ test('should return default config from package', t => {
 				perfectionist: {},
 				'postcss-discard-comments': {}
 			}
-		},
+		}
+	};
+
+	t.deepEqual(expected.postcss, postcss);
+});
+
+test('should return default config posthtml from package', t => {
+	const {posthtml} = postConfig();
+	const expected = {
 		posthtml: {
 			sync: true,
 			plugins: {
@@ -76,7 +86,201 @@ test('should return default config from package', t => {
 		}
 	};
 
-	t.deepEqual(expected, {posthtml: posthtml, postcss: postcss});
+	t.deepEqual(expected.posthtml, posthtml);
+});
+
+test('should return default config posthtml with extends config', t => {
+	const extend = {
+		posthtml: {
+			sync: false,
+			plugins: {
+				'posthtml-remove-tags': {
+					tags: 'a'
+				},
+				beautify: {
+					rules: {
+						indent: 4
+					}
+				}
+			}
+		},
+		postcss: {
+			from: 'src/',
+			plugins: {
+				'postcss-calc': {
+					precision: 2
+				}
+			}
+		},
+		styleToFile: {
+			path: './*.css'
+		},
+		plugins: {
+			atRulesVariables: {
+				atRule: ['@test']
+			}
+		}
+	};
+	const {posthtml} = postConfig(extend);
+	const expected = {
+		posthtml: {
+			sync: false,
+			plugins: {
+				'posthtml-bem': {
+					elemPrefix: '__',
+					modPrefix: '--',
+					modDlmtr: '-'
+				},
+				'posthtml-style-to-file': {
+					path: './*.css'
+				},
+				'posthtml-modules': {
+					root: './src/'
+				},
+				'posthtml-inline-assets': {
+					from: 'dev/index.html'
+				},
+				'posthtml-remove-tags': {
+					tags: 'a'
+				},
+				'posthtml-css-modules': {},
+				'posthtml-each': {},
+				'posthtml-include': {},
+				'posthtml-beautify': {
+					rules: {
+						indent: 4
+					}
+				},
+				'posthtml-inline-css': {},
+				'posthtml-remove-attributes': 'class'
+			}
+		}
+	};
+
+	t.deepEqual(expected.posthtml, posthtml);
+});
+
+test('should return default config postcss with extends config', t => {
+	const extend = {
+		posthtml: {
+			sync: false,
+			plugins: {
+				'posthtml-remove-tags': {
+					tags: 'a'
+				},
+				beautify: {
+					rules: {
+						indent: 4
+					}
+				}
+			}
+		},
+		postcss: {
+			from: 'src/',
+			plugins: {
+				'postcss-calc': {
+					precision: 2
+				}
+			}
+		},
+		styleToFile: {
+			path: './*.css'
+		},
+		plugins: {
+			atRulesVariables: {
+				atRule: ['@test']
+			}
+		}
+	};
+	const {postcss} = postConfig(extend);
+	const expected = {
+		postcss: {
+			from: 'src/',
+			to: './',
+			plugins: {
+				'postcss-modules': {
+					generateScopedName: '[name]__[local]___[hash:base64:5]'
+				},
+				autoprefixer: {
+					browsers: ['last 2 versions']
+				},
+				'postcss-at-rules-variables': {
+					atRule: ['@test', '@media']
+				},
+				'postcss-attribute-selector-prefix': {},
+				'postcss-banner': {},
+				'postcss-browser-reporter': {},
+				'postcss-calc': {
+					precision: 2
+				},
+				'postcss-class-prefix': {},
+				'postcss-clearfix': {},
+				'postcss-conditionals': {},
+				'postcss-csso': {},
+				'postcss-custom-properties': {},
+				'postcss-devtools': {},
+				'postcss-each': {},
+				'postcss-easy-import': {},
+				'postcss-extend': {},
+				'postcss-for': {},
+				'postcss-initial': {},
+				'postcss-mixins': {},
+				'postcss-nested': {},
+				'postcss-sorting': {},
+				cssnano: {},
+				perfectionist: {},
+				'postcss-discard-comments': {}
+			}
+		}
+	};
+
+	t.deepEqual(expected.postcss, postcss);
+});
+
+test('should return default config postcss with extends config from file', t => {
+	const {postcss} = postConfig('fixtures/postcss-config-extends.json');
+	const expected = {
+		postcss: {
+			from: 'src/',
+			to: './',
+			plugins: {
+				'postcss-modules': {
+					generateScopedName: '[name]__[local]___[hash:base64:5]'
+				},
+				autoprefixer: {
+					browsers: ['last 2 versions']
+				},
+				'postcss-at-rules-variables': {
+					atRule: ['@test', '@media']
+				},
+				'postcss-attribute-selector-prefix': {},
+				'postcss-banner': {},
+				'postcss-browser-reporter': {},
+				'postcss-calc': {
+					precision: 2
+				},
+				'postcss-class-prefix': {},
+				'postcss-clearfix': {},
+				'postcss-conditionals': {},
+				'postcss-csso': {},
+				'postcss-custom-properties': {},
+				'postcss-devtools': {},
+				'postcss-each': {},
+				'postcss-easy-import': {},
+				'postcss-extend': {},
+				'postcss-for': {},
+				'postcss-initial': {},
+				'postcss-mixins': {},
+				'postcss-nested': {},
+				'postcss-sorting': {},
+				cssnano: {},
+				perfectionist: {},
+				'postcss-discard-comments': {}
+			}
+		}
+	};
+
+	t.deepEqual(expected.postcss, postcss);
 });
 
 /* test('should return object witch five params from package.json', t => {
