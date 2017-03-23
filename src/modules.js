29a23403fb7a58bf8330170ea8278a5907f5bf51
Ivan Demidov <demidov@220-volt.ru>
2017-03-23 15:32:29 +0300

    style(modules): fix comment

--- src/modules.js
+++ src/modules.js
@@ -16,7 +16,7 @@ export default new class {
 
 	find(namespace = '', property) {
 		return this.list.find(module => {
-			// fix, waits resolve issue #30
+			// Fix, waits resolve issue #30
 			if (property === '0' || property === '1') {
 				return false;
 			}
